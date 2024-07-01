0d362784ea20bd49179d2c040bcd338d060855d8
AbiolaR <rasheedabi97@hotmail.com>
2024-07-01 20:11:20 +0200

    feat: backend init

---
+++ backend/src/main/java/com/europace_task/tilgungsplan/TilgungsplanApplication.java
@@ -0,0 +1,13 @@
+package com.europace_task.tilgungsplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilgungsplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilgungsplanApplication.class, args);
+	}
+
+}
